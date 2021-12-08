8acbb118a4fc8f933fee4dfbd9cb5182c2853db8
juan manuel gomez <jmgomezn8you@example.com>
2021-12-07 21:39:48 -0300

    tarea 4 ok

--- js/card.js
+++ js/card.js
@@ -1,4 +1,4 @@
-function initInicio() {
+/* function initInicio() {
 
     function Card(heading, description, image, btn, price) {
         this.heading = heading
@@ -21,41 +21,31 @@ function initInicio() {
                 this.classList.toggle('card--selected')
                 elemSectionCart.innerHTML += `<div class="cart__item">${that.heading}</div>`
             })
-            newCard.innerHTML = 
-                `
-                <input type="checkbox" id="card${this.btn}" class="card__input" aria-hidden="true">
-                <div class="card__content">
-                    <div class="card__image" style="background-image: url(${this.image})">
-                        <div class="card__front">
-                            <h3 class="card__heading">
-                                ${this.heading} 
-                            </h3>
-                            <label for="card${this.btn}" class="card__button" style="background-image: url(/img/iconos/ir.png);" aria-hidden="true">
-                            </label>
-                        </div>
-                    </div>
-                    <div class="card__back">
-                        <div class="card__back-inner">
-                            <div class="card__back-description">
-                                <p>
-                                    ${this.description}
-                                </p>
-                            </div>
-                            <div class="card__back-price">
-                                $${this.price}
-                            </div>
-                            <label for="card${this.btn}" class="card__back-button" style="background-image: url(/img/iconos/retorno.png);" aria-hidden="true">
-                            </label>
-                        </div>
-                    </div>
-                </div>
-
-                `
-            destinationElement.appendChild(newCard)
+            
+            function renderProds() {
+        
+                const xhr = new XMLHttpRequest
+                xhr.open('get', 'plantillas/cards.hbs')
+                xhr.addEventListener('load', () => {
+                    if(xhr.status == 200) {
+                        let plantillaHbs = xhr.response
+                        var template = Handlebars.compile(plantillaHbs);
+                        let html = template({cards})
+                        newCard.innerHTML = html
+                        
+                    }
+                })
+                xhr.send()
+            }
+            
+            renderProds()
+            
+            elemCardsContainer.appendChild(newCard)
         }
-
+        
     }
 
+    
     var elemCardsContainer = document.getElementsByClassName('cards-container')[0]
 
     var cards = [
@@ -80,8 +70,61 @@ function initInicio() {
     ]
 
 
+    
+
     for (var card of cards) {
         card.appendTo(elemCardsContainer)
     }
+
+}
+
+ */
+
+function initInicio() {
+
+
+
+    const cards = [
+        {heading: 'Iphone 11', description: 'bla bla bla', image: 'img/productos/apple-iphone-11-pro-max.jpg', btn: 1, price: 999},
+        {heading: 'Iphone 12', description: 'bla bla bla', image: 'img/productos/apple-iphone-12.jpg', btn: 2, price: 999},
+        {heading: 'Sony Mdr-Zx110', description: 'bla bla bla', image: 'img/productos/auriculares-sony.jpg', btn: 3, price: 999},
+        {heading: 'Canon PowerShot SX', description: 'bla bla bla', image: 'img/productos/camara-canon.jpg', btn: 4, price: 999},
+        {heading: 'Samsung Galaxy S-21', description: 'bla bla bla', image: 'img/productos/celular-samsung.jpg', btn: 5, price: 999},
+        {heading: 'Chromecast', description: 'bla bla bla', image: 'img/productos/chromecast-google.jpg', btn: 6, price: 999},
+        {heading: 'DJI Mavic Pro', description: 'bla bla bla', image: 'img/productos/dji-mavic-2-pro.jpg', btn: 7, price: 999},
+        {heading: 'DJI Mavic 2 Pro', description: 'bla bla bla', image: 'img/productos/drone-dji.jpg', btn: 8, price: 999},
+        {heading: 'Google Nest Mini', description: 'bla bla bla', image: 'img/productos/google-nest-mini.jpg', btn: 9, price: 999},
+        {heading: 'Lavarropas DH', description: 'bla bla bla', image: 'img/productos/lavarropas-dh.jpg', btn: 10, price: 999},
+        {heading: 'Modem Motorola SBG675', description: 'bla bla bla', image: 'img/productos/modem-motorola.jpg', btn: 11, price: 999},
+        {heading: 'Motorola Moto e7', description: 'bla bla bla', image: 'img/productos/motorola-moto-e7.jpg', btn: 12, price: 999 },
+        {heading: 'Notebook Lenovo Core I7', description: 'bla bla bla', image: 'img/productos/notebook-lenovo.jpg', btn: 13, price: 999 },
+        {heading: 'Parlante JBL', description: 'bla bla bla', image: 'img/productos/parlante-jbl.jpg', btn: 14, price: 999 },
+        {heading: 'Samsung Galaxy s21', description: 'bla bla bla', image: 'img/productos/samsung-galaxy-s21-plus-5g.jpg', btn: 15, price: 999 },
+        {heading: 'Samsung tu7000', description: 'bla bla bla', image: 'img/productos/samsung-tu7000.jpg', btn: 16, price: 999 },
+        {heading: 'Sony Playstation 5', description: 'bla bla bla', image: 'img/productos/sony-playstation-5.jpg', btn: 17, price: 999 },
+        {heading: 'Televisor LG', description: 'bla bla bla', image: 'img/productos/televisor-lg.jpg', btn: 18, price: 999 },
+    ]
+
+    console.log(cards[0].heading)
+    const elemCardsContainer = document.getElementsByClassName('cards-container')[0]
+
+    function renderProds() {
+            
+        const xhr = new XMLHttpRequest
+        xhr.open('get', 'plantillas/cards.hbs')
+        xhr.addEventListener('load', () => {
+            if(xhr.status == 200) {
+                let plantillaHbs = xhr.response
+                var template = Handlebars.compile(plantillaHbs);
+                let html = template({cards})
+                elemCardsContainer.innerHTML += html                
+            }
+        })
+        xhr.send()
+    }
+
+    renderProds()
+
+    
 
 }
